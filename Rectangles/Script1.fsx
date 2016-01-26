d84e86c5644e00e309657bb3b8ba3f1d436cc69e
htoma <horia.toma@availpro.com>
2016-01-26 11:50:16 +0100

    Intersect rectangles

---
+++ Rectangles/Script1.fsx
@@ -0,0 +1,34 @@
+﻿open System
+
+type Rectangle =
+    { Top: int
+      Left: int
+      Width: int
+      Height: int }
+
+let intersect (first:Rectangle) (second:Rectangle) = 
+    if (first.Left + first.Width < second.Left) ||
+        (second.Left + second.Width < first.Left) ||
+        (first.Top + first.Height < second.Top) ||
+        (second.Top + second.Height < first.Top) then None
+    else
+        let left = Math.Max(first.Left, second.Left)
+        let top = Math.Max(first.Top, second.Top)
+        Some(
+            { Left=left
+              Top=top
+              Width=Math.Min(first.Left+first.Width, second.Left+second.Width)-left
+              Height=Math.Min(first.Top+first.Height, second.Top+second.Height)-top
+            })
+
+let first = { Left=8
+              Top=8
+              Width=4
+              Height=4 }
+
+let second = { Left=8
+               Top=8
+               Width=4
+               Height=4 }
+
+intersect first second
